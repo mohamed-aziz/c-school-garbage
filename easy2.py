aed0eb115c4094dda58e9bc4f9b11454fdf6ece9
mohamed-aziz <medazizknani@gmail.com>
2017-02-08 10:40:55 +0000

    Untracking some files.

--- easy2.py
+++
@@ -1,6 +0,0 @@
-# print('\n'.join(map(lambda x: str(ord(x)), [input() for _ in range(int(input()))][::-1])))
-
-a = [('Mohamed', 1), ('', -1), ('', 3), ('', 4)]
-a.sort(key=lambda x: x[1])
-print(a)
-
--- exemple.py
+++
@@ -1,2 +0,0 @@
-s1, s2 = [''.join(list(filter(lambda x: x!=' ', list(input())))) for _ in range(2)]
-print('Palidromes' if s1 == s2[::-1] else 'Non palindromes')
